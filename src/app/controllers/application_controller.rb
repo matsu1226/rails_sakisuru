462e2b329316b8f458595fa8769907fa89ee2ee4
sho matsuda <qqq.ms1126@gmail.com>
2021-10-04 21:59:45 +0900

    user_newのビュー作成 #3

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # deviseにおけるstrong_parameterの追加 https://qiita.com/you8/items/1781029c5d14aa23bded
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthday])
+  end
+
 end
