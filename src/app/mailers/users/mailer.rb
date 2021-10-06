fcb92219b7725d9a87da4153174fe49bf01daf3a
sho matsuda <qqq.ms1126@gmail.com>
2021-10-06 11:03:48 +0900

    mailer_specの通過 #12

--- src/app/mailers/users/mailer.rb
+++ src/app/mailers/users/mailer.rb
@@ -5,16 +5,16 @@ class Users::Mailer < Devise::Mailer
   default template_path: 'devise/mailer'
 
   def confirmation_instructions(record, token, opts={})
-    #record内にユーザ情報が入っていました。そこの"unconfirmed_email"の有無で登録／変更を仕分けます
+    # record =  <User id: 6, 
+    #                 email: "qqq.ms1121@gmail.com", 
+    #                 created_at: "2021-10-05 14:03:33.545647000 +0000",
+    #                 updated_at: "2021-10-05 14:03:33.545647000 +0000", 
+    #                 name: "松田　正太郎", 
+    #                 birthday: "1990-01-01">
     #opts属性を上書きすることで、Subjectやfromなどのヘッダー情報を変更することが可能！
-    if record.unconfirmed_email != nil
-      opts[:subject] = "【サキスル／目標管理サービス】仮登録メール（再送）"
-    # else
-      opts[:subject] = "【サキスル／目標管理サービス】仮登録メール"
-    # end
+    opts[:subject] = "【サキスル／目標管理サービス】仮登録メール"
+    
     #件名の指定以外は親を継承
     super
   end
-
 end
-
